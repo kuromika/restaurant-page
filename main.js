0fb6fc35f687984d1226b074e76e9da9706d6279
mika-alpha <yuukeikioku@gmail.com>
2022-08-16 14:37:08 -0500

    Change colors

--- main.js
+++ main.js
@@ -16,7 +16,7 @@
   \*************************************************************/
 /***/ ((module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/getUrl.js */ \"./node_modules/css-loader/dist/runtime/getUrl.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__);\n// Imports\n\n\n\nvar ___CSS_LOADER_URL_IMPORT_0___ = new URL(/* asset import */ __webpack_require__(/*! ./SignikaNegative-VariableFont_wght.ttf */ \"./src/SignikaNegative-VariableFont_wght.ttf\"), __webpack_require__.b);\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\nvar ___CSS_LOADER_URL_REPLACEMENT_0___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default()(___CSS_LOADER_URL_IMPORT_0___);\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, \"@font-face {\\n    font-family: 'Signika';\\n    src: url(\" + ___CSS_LOADER_URL_REPLACEMENT_0___ + \") format('tff');\\n    font-weight: 600;\\n    font-style: normal;\\n}\\n\\n*{\\n    box-sizing: border-box;\\n    margin: 0;\\n    padding: 0;\\n    font-family: 'Signika';\\n}\\n\\n#content{\\n    display: flex;\\n    flex-direction: column;\\n    align-items: center;\\n    gap: 30px;\\n    background-color: #FEFAE0;\\n    height: 100vh;\\n}\\n\\n.header{\\n    background-color: #CCD5AE;\\n    width: 100%;\\n    height: 100px;\\n    display: flex;\\n    justify-content: center;\\n    align-items: center;\\n    gap: 20px;\\n    padding: 5;\\n}\\n\\nbutton{\\n    background-color: #D4A373;\\n    color: #FEFAE0;\\n    width: 100px;\\n    height: 50px;\\n    font-size: 17px;\\n    font-weight: 900;\\n}\\n\\n.container{\\n    background-color: #FAEDCD;\\n    padding: 20px;\\n    border: 5px solid;\\n    border-color: #CCD5AE;\\n    max-width: 80%;\\n}\", \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://restaurant-page/./src/style.css?./node_modules/css-loader/dist/cjs.js");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/getUrl.js */ \"./node_modules/css-loader/dist/runtime/getUrl.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__);\n// Imports\n\n\n\nvar ___CSS_LOADER_URL_IMPORT_0___ = new URL(/* asset import */ __webpack_require__(/*! ./SignikaNegative-VariableFont_wght.ttf */ \"./src/SignikaNegative-VariableFont_wght.ttf\"), __webpack_require__.b);\nvar ___CSS_LOADER_URL_IMPORT_1___ = new URL(/* asset import */ __webpack_require__(/*! ./bg.png */ \"./src/bg.png\"), __webpack_require__.b);\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\nvar ___CSS_LOADER_URL_REPLACEMENT_0___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default()(___CSS_LOADER_URL_IMPORT_0___);\nvar ___CSS_LOADER_URL_REPLACEMENT_1___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default()(___CSS_LOADER_URL_IMPORT_1___);\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, \"@font-face {\\n    font-family: 'Signika';\\n    src: url(\" + ___CSS_LOADER_URL_REPLACEMENT_0___ + \") format('tff');\\n    font-weight: 600;\\n    font-style: normal;\\n}\\n\\n*{\\n    box-sizing: border-box;\\n    margin: 0;\\n    padding: 0;\\n    font-family: 'Signika';\\n}\\n\\nbody{\\n    height: 167vh;\\n}\\n\\n\\n.header{\\n    background-color: #57CC99;\\n    width: 100%;\\n    height: 100px;\\n    display: flex;\\n    justify-content: center;\\n    align-items: center;\\n    gap: 20px;\\n    padding: 5px;\\n}\\n\\nbutton{\\n    background-color: #38A3A5;\\n    color: #FEFAE0;\\n    width: 100px;\\n    height: 50px;\\n    font-size: 17px;\\n    font-weight: 900;\\n    border: 2px solid #C7F9CC;\\n}\\n\\nbutton:active{\\n    background-color: #C7F9CC;\\n}\\n\\n.container{\\n    display: flex;\\n    justify-content: center;\\n    background-color: #C7F9CC;\\n    padding: 20px;\\n    border: 5px solid;\\n    border-color: #57CC99;\\n}\\n\\n#content{\\n    display: flex;\\n    flex-direction: column;\\n    align-items: center;\\n    gap: 30px;\\n    background: url(\" + ___CSS_LOADER_URL_REPLACEMENT_1___ + \");\\n    background-repeat: repeat;\\n    background-size: 100%;\\n    height: 100%;\\n    width: 100%;\\n}\\n\\n.item{\\n    display: flex;\\n    flex-direction: column;\\n    align-items: center;\\n    justify-content: center;\\n    gap: 30px;\\n\\n}\\n\\n.item img{\\n    width: 300px;\\n    height: 200px;\\n    border: 3px dashed #57CC99;\\n}\\n\\n\", \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://restaurant-page/./src/style.css?./node_modules/css-loader/dist/cjs.js");
 
 /***/ }),
 
@@ -136,7 +136,67 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _loa
   \***********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"createAbout\": () => (/* binding */ createAbout),\n/* harmony export */   \"createHeader\": () => (/* binding */ createHeader),\n/* harmony export */   \"wrapInContainer\": () => (/* binding */ wrapInContainer)\n/* harmony export */ });\nlet contentDiv = document.getElementById('content');\n\nfunction createAbout(){\n\n    let title = document.createElement('h1');\n    title.textContent = \"ERBS restaurant\";\n\n    let about = document.createElement('p');\n    about.textContent = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa expedita repellendus corporis asperiores eaque tempora. Velit nam maxime illo soluta facere sapiente quam quis, sunt consequatur quae. Expedita, corporis aliquam?';\n\n\n    contentDiv.append(wrapInContainer(title));\n    contentDiv.append(wrapInContainer(about));\n}\n\nfunction wrapInContainer(element){\n    let container = document.createElement('div');\n    container.classList.add('container');\n    container.append(element);\n    return container;\n}\n\nfunction createHeader(){\n    let header = document.createElement('header');\n    header.classList.add('header');\n    header.append(createButton('HOME'));\n    header.append(createButton('MENU'));\n    header.append(createButton('ABOUT'));\n    contentDiv.append(header);\n}\n\nfunction createButton(content){\n    let button = document.createElement('button');\n    button.setAttribute('type', 'button');\n    button.classList.add('nav');\n    button.textContent = content;\n    return button;\n}\n\n\n\n//# sourceURL=webpack://restaurant-page/./src/loader.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"bodyDiv\": () => (/* binding */ bodyDiv),\n/* harmony export */   \"createAbout\": () => (/* binding */ createAbout),\n/* harmony export */   \"createHeader\": () => (/* binding */ createHeader),\n/* harmony export */   \"wrapInContainer\": () => (/* binding */ wrapInContainer)\n/* harmony export */ });\n/* harmony import */ var _menu_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./menu.js */ \"./src/menu.js\");\n\n\nlet contentDiv = document.getElementById('content');\n\nlet bodyDiv = document.createElement('div');\n\nfunction createAbout(){\n\n    bodyDiv.replaceChildren();\n\n    let title = document.createElement('h1');\n    title.textContent = \"ERBS restaurant\";\n\n    let about = document.createElement('p');\n    about.textContent = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa expedita repellendus corporis asperiores eaque tempora. Velit nam maxime illo soluta facere sapiente quam quis, sunt consequatur quae. Expedita, corporis aliquam?';\n\n    bodyDiv.append(wrapInContainer(title));\n    bodyDiv.append(wrapInContainer(about));\n    contentDiv.append(bodyDiv);\n}\n\nfunction wrapInContainer(element){\n    let container = document.createElement('div');\n    container.classList.add('container');\n    container.append(element);\n    return container;\n}\n\nfunction createHeader(){\n    const header = document.createElement('header');\n    header.classList.add('header');\n\n    const home = createButton('HOME', createAbout);\n    header.append(home);\n\n    const menu = createButton('MENU', _menu_js__WEBPACK_IMPORTED_MODULE_0__.populateMenu);\n    header.append(menu);\n\n    const contact = createButton('CONTACT');\n    header.append(contact);\n\n    contentDiv.append(header);\n}\n\nfunction createButton(content, action){\n    let button = document.createElement('button');\n    button.setAttribute('type', 'button');\n    button.classList.add('nav');\n    button.textContent = content;\n    button.addEventListener('click', action);\n    return button;\n}\n\n\n\n//# sourceURL=webpack://restaurant-page/./src/loader.js?");
+
+/***/ }),
+
+/***/ "./src/menu.js":
+/*!*********************!*\
+  !*** ./src/menu.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"populateMenu\": () => (/* binding */ populateMenu)\n/* harmony export */ });\n/* harmony import */ var _loader__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./loader */ \"./src/loader.js\");\n/* harmony import */ var _035_Scrambled_Egg_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./035. Scrambled Egg_스크램블 에그.png */ \"./src/035. Scrambled Egg_스크램블 에그.png\");\n/* harmony import */ var _040_Fried_Chicken_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./040. Fried Chicken_후라이드 치킨.png */ \"./src/040. Fried Chicken_후라이드 치킨.png\");\n/* harmony import */ var _044_Steak_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./044. Steak_스테이크.png */ \"./src/044. Steak_스테이크.png\");\n/* harmony import */ var _053_Fish_And_Chips_png__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./053. Fish And Chips_피쉬 앤 칩스.png */ \"./src/053. Fish And Chips_피쉬 앤 칩스.png\");\n/* harmony import */ var _054_11_Combo_Meal_png__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./054. #11 Combo Meal_일레븐 세트.png */ \"./src/054. \\u0000#11 Combo Meal_일레븐 세트.png\");\n\n\n\n\n\n\n\n\nlet menu = [_035_Scrambled_Egg_png__WEBPACK_IMPORTED_MODULE_1__, _040_Fried_Chicken_png__WEBPACK_IMPORTED_MODULE_2__, _044_Steak_png__WEBPACK_IMPORTED_MODULE_3__, _053_Fish_And_Chips_png__WEBPACK_IMPORTED_MODULE_4__, _054_11_Combo_Meal_png__WEBPACK_IMPORTED_MODULE_5__];\nlet descriptions = ['A common\\n\\n egg variety, competing with sunny-side-up. When you have no butter or milk, why not scramble an egg?',\n                    \"If it can fly and has wings and a beak, it's definitely a chicken. It could actually be a crow, pigeon, or whatever, but once you fry it, it will still be Fried Chicken.\",\n                    \"A tasty piece of meat from a unkown animal\",\n                    \"The favorite meal of many british people\",\n                    \"Everyone's favorite combo meal prepared by Eleven herself.\"];\n\nfunction createMenuItem(item, index){\n    const menuItem = document.createElement('div');\n    const description = document.createElement('h4');\n    description.textContent = descriptions[index];\n    const food = new Image();\n    food.src = item;\n    menuItem.append(description);\n    menuItem.append(food);\n    menuItem.classList.add('item');\n    _loader__WEBPACK_IMPORTED_MODULE_0__.bodyDiv.append((0,_loader__WEBPACK_IMPORTED_MODULE_0__.wrapInContainer)(menuItem));\n}\n\nfunction populateMenu(){\n    _loader__WEBPACK_IMPORTED_MODULE_0__.bodyDiv.replaceChildren();\n    for (let i = 0; i < menu.length; i++){\n        createMenuItem(menu[i], i);\n    }\n}\n\n\n\n//# sourceURL=webpack://restaurant-page/./src/menu.js?");
+
+/***/ }),
+
+/***/ "./src/035. Scrambled Egg_스크램블 에그.png":
+/*!********************************************!*\
+  !*** ./src/035. Scrambled Egg_스크램블 에그.png ***!
+  \********************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"2a197f44f6090c7aea8c.png\";\n\n//# sourceURL=webpack://restaurant-page/./src/035._Scrambled_Egg_%EC%8A%A4%ED%81%AC%EB%9E%A8%EB%B8%94_%EC%97%90%EA%B7%B8.png?");
+
+/***/ }),
+
+/***/ "./src/040. Fried Chicken_후라이드 치킨.png":
+/*!********************************************!*\
+  !*** ./src/040. Fried Chicken_후라이드 치킨.png ***!
+  \********************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"d9405379148fe7df120d.png\";\n\n//# sourceURL=webpack://restaurant-page/./src/040._Fried_Chicken_%ED%9B%84%EB%9D%BC%EC%9D%B4%EB%93%9C_%EC%B9%98%ED%82%A8.png?");
+
+/***/ }),
+
+/***/ "./src/044. Steak_스테이크.png":
+/*!*********************************!*\
+  !*** ./src/044. Steak_스테이크.png ***!
+  \*********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"9124c386062cb3a3e44d.png\";\n\n//# sourceURL=webpack://restaurant-page/./src/044._Steak_%EC%8A%A4%ED%85%8C%EC%9D%B4%ED%81%AC.png?");
+
+/***/ }),
+
+/***/ "./src/053. Fish And Chips_피쉬 앤 칩스.png":
+/*!*********************************************!*\
+  !*** ./src/053. Fish And Chips_피쉬 앤 칩스.png ***!
+  \*********************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"fe08cf97e3d2beced0c9.png\";\n\n//# sourceURL=webpack://restaurant-page/./src/053._Fish_And_Chips_%ED%94%BC%EC%89%AC_%EC%95%A4_%EC%B9%A9%EC%8A%A4.png?");
+
+/***/ }),
+
+/***/ "./src/054. \u0000#11 Combo Meal_일레븐 세트.png":
+/*!*********************************************!*\
+  !*** ./src/054.  #11 Combo Meal_일레븐 세트.png ***!
+  \*********************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"1dfeccd9599e9c37e779.png\";\n\n//# sourceURL=webpack://restaurant-page/./src/054._%00#11_Combo_Meal_%EC%9D%BC%EB%A0%88%EB%B8%90_%EC%84%B8%ED%8A%B8.png?");
 
 /***/ }),
 
@@ -147,6 +207,16 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 eval("module.exports = __webpack_require__.p + \"af02c6832b27eb96b55a.ttf\";\n\n//# sourceURL=webpack://restaurant-page/./src/SignikaNegative-VariableFont_wght.ttf?");
+
+/***/ }),
+
+/***/ "./src/bg.png":
+/*!********************!*\
+  !*** ./src/bg.png ***!
+  \********************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"a020e1eda414afc60a1b.png\";\n\n//# sourceURL=webpack://restaurant-page/./src/bg.png?");
 
 /***/ })
 
